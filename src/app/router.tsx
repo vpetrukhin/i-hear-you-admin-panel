d6a4508f89684a45a3c7451fc9473dd34dc5c562
Anastasia <gatta.gizmo@gmail.com>
2025-09-29 19:21:25 +0300

    fixes

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,6 +4,7 @@ import {
   MaterialListPage,
 } from "@/pages/MaterialListPage";
 import { MATERIAL_PAGE_ROUTE, MaterialPage } from "@/pages/MaterialPage";
+import { MATERIAL_CREATE_PAGE_ROUTE, MaterialCreatePage } from '@/pages/MaterialCreatePage';
 import { createBrowserRouter, Navigate } from "react-router";
 
 export const router = createBrowserRouter([
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
   {
     path: MATERIAL_LIST_PAGE_ROUTE,
     element: <MaterialListPage />,
+  },
+  {
+    path: MATERIAL_CREATE_PAGE_ROUTE,
+    element: <MaterialCreatePage />
   },
   {
     path: MATERIAL_PAGE_ROUTE,
