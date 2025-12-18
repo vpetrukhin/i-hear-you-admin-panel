5d7d02bf6c3df1a8443dd10f3e2bb18b77e6ecaa
Vasya Petrukhin <>
2025-12-18 17:57:58 +0300

    Изменен максимальная длина слага

--- src/shared/lib/getSlug.ts
+++ src/shared/lib/getSlug.ts
@@ -1,6 +1,6 @@
 import slugify from 'slugify';
 
-export const getSlug = (value: string, maxLength = 19) => slugify(Array.from(value).slice(0, maxLength).join(''), {
+export const getSlug = (value: string, maxLength = 15) => slugify(Array.from(value).slice(0, maxLength).join(''), {
   lower: true,        // lowercase
   strict: true,       // убрать спецсимволы
   trim: true,
